d50c15e3e6102bbfb8aa2137528bb5dd255da48e
anasemadanas <anasemadanas1@gmail.com>
2026-05-05 00:05:00 +0300

    FIX PYTHONWHERE

--- money_manager_web/expenses/views.py
+++ money_manager_web/expenses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -20,18 +21,15 @@ def home(request):
     return render(request, "login.html")
 
 
+
 def logout_view(request):
     logout(request)
     return redirect("login")
 
 
 
-
-
-
+@login_required(login_url="login")
 def dashboard(request):
-    if not request.user.is_authenticated:
-        return redirect("login")
 
     context = {
         "username": request.user.username,
